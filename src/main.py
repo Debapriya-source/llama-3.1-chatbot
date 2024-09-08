3c168618f4874c04f084d469da61fe1fa386d8b7
Debapriya Das <debapriyadas73@gmail.com>
2024-09-08 18:12:19 +0530

    update the context

--- src/main.py
+++ src/main.py
@@ -1,40 +1,61 @@
 import os
 import json
 
+from dotenv import dotenv_values
+
 import streamlit as st
 from groq import Groq
 
 
+def parse_groq_stream(stream):
+    for chunk in stream:
+        if chunk.choices:
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
+
+
 # streamlit page configuration
 st.set_page_config(
-    page_title="llama-3.1 Chatbot",
-    page_icon="🦙",
+    page_title="My Coding Buddy",
+    page_icon="🤖",
     layout="centered",
 )
 
-working_dir = os.path.dirname(os.path.abspath(__file__))
-config_data = json.load(open(f"{working_dir}/config.json"))
 
-GROQ_API_KEY = config_data["GROQ_API_KEY"]
+try:
+    secrets = dotenv_values(".env")  # for dev env
+    GROQ_API_KEY = secrets["GROQ_API_KEY"]
+except:
+    secrets = st.secrets  # for streamlit deployment
+    GROQ_API_KEY = secrets["GROQ_API_KEY"]
 
 # save the api_key to environment variable
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+INITIAL_RESPONSE = secrets["INITIAL_RESPONSE"]
+INITIAL_MSG = secrets["INITIAL_MSG"]
+CHAT_CONTEXT = secrets["CHAT_CONTEXT"]
+
 
 client = Groq()
 
 # initialize the chat history if present as streamlit session
 if "chat_history" not in st.session_state:
     # print("message not in chat session")
-    st.session_state.chat_history = []
+    st.session_state.chat_history = [
+        {"role": "assistant",
+         "content": INITIAL_RESPONSE
+         },
+    ]
 
 # page title
-st.title("Welcome!")
-
+st.title("Welcome Buddy🤓!")
+st.caption("Helping You Level Up Your Coding Game")
 # the messages in chat_history will be stored as {"role":"user/assistant", "content":"msg}
 # display chat history
 for message in st.session_state.chat_history:
     # print("message in chat session")
-    with st.chat_message("role"):
+    with st.chat_message("role", avatar='🤖'):
         st.markdown(message["content"])
 
 
@@ -43,27 +64,26 @@ user_prompt = st.chat_input("Ask me")
 
 if user_prompt:
     # st.chat_message("user").markdown
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar="🗨️"):
         st.markdown(user_prompt)
     st.session_state.chat_history.append(
         {"role": "user", "content": user_prompt})
 
     # get a response from the LLM
     messages = [
-        {"role": "assistant", "content": "You are a DSA teacher"},
+        {"role": "system", "content": CHAT_CONTEXT
+         },
+        {"role": "assistant", "content": INITIAL_MSG},
         *st.session_state.chat_history
     ]
 
-    response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
-        messages=messages,
-    )
-
-    assistant_response = response.choices[0].message.content
+    # Display assistant response in chat message container
+    with st.chat_message("assistant", avatar='🤖'):
+        stream = client.chat.completions.create(
+            model="llama-3.1-8b-instant",
+            messages=messages,
+            stream=True  # for streaming the message
+        )
+        response = st.write_stream(parse_groq_stream(stream))
     st.session_state.chat_history.append(
-        {"role": "assistant", "content": assistant_response})
-
-    # display llm response
-    with st.chat_message("assistant"):
-        # st.markdown(assistant_response)
-        st.markdown(assistant_response)
+        {"role": "assistant", "content": response})
